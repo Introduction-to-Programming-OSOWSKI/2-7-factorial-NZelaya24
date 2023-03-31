3a6787e49175d05e02da7056eebba0702f8c919a
NZelaya24 <825123@mystma.org>
2023-03-31 14:20:35 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #WRITE YOUR CODE IN THIS FILE
+def factorial (x):
+    num = 1 
+    for i in range (1 , x + 1 ):
+        num = num * i  
+    return num 
+   
+    
+        
+
+    
+
+
+factorial (5)
+factorial (10)
+
+
+
